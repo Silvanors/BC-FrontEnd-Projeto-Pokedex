6bf35998feab1124344a028c0b05155f522223eb
silvano rodrigues <silvanors@gmail.com>
2023-12-26 23:05:07 -0300

    implementação do fetch

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ instalar a biblioteca "npm instal -g http-server";</br>
 Dribbble: Site de modelelos de layout https://dribbble.com/shots/6540871-Pokedex-App;</br>
 API: https://pokeapi.co/docs/v2#pokemon;</br>
 Site W3school para tutoriais de CSS https://www.w3schools.com/css/css3_mediaqueries_ex.asp;</br>
-testes
+Uso do "fetch API GET with json": "https://developer.mozilla.org/en-US/docs/Web/API/Response/json;</br>
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,42 @@
-console.log("Sucesso!");
+//console.log("Sucesso!");
+
+const offset = 0;
+const limit = 10;
+
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+//Usa-se o "Fetch para fazer a requisição" passa-se a URL ou Request
+fetch(url)
+    /*.then(function(response){
+        //console.log(response)
+        response
+            .json()
+            .then(function (responseBody){
+            console.log(responseBody)
+            })
+    })*/
+    
+    /* o "then" pode ser encadeado e o "then" seguinte aguarda a resposta do anterior*/
+   
+   /* .then(function (response){
+        return response.json()
+    }) esta função foi transformada em eron function!!! abaixo*/
+
+    .then((response) => response.json())
+
+    .then((jsonBody) => console.log(jsonBody))
+    
+    .catch((error) => console.error(error))
+
+    .finally(function(){
+        console.log('Requisição concluída')
+    })
+
+
+const x = 10 + 10;
+console.log(x)
+//Este fetch retorna uma "promise" para lidar com o assincronismo no js.
+//o retorno é uma promessa de resultado devido ao processo de requisição e resposta do 
+//servidor
+
+
